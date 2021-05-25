cf2fd4ba31a3157c41e17046a1c81d655858fc9c
Sree Ranjani M <sreeranjani.m@thoughtworks.com>
2021-05-25 11:19:12 +0530

    Adds test case for empty list

--- src/head.spec.js
+++ src/head.spec.js
@@ -5,4 +5,8 @@ describe('Head', () => {
     it('Head of list [1,2,3] is 1', () => {
         expect(head([1,2,3])).toEqual(1);
     });
+
+    it('Head of list [] is null or undefined', () => {
+        expect(head([])).toEqual(null || undefined);
+    })
 })
