b1c549d3693232acfe485db9f522b7ab5e68d1e1
Sree Ranjani M <sreeranjani.m@thoughtworks.com>
2021-05-25 11:59:26 +0530

    Adds test case for empty list in tail

--- src/tail.js
+++ src/tail.js
@@ -1,5 +1,7 @@
 var list = [1,2,3]
 const tail = (list) => {
+    if(list.length == 0)
+        return undefined;
     return list.slice(1);
 }
 module.exports = tail;
--- src/tail.spec.js
+++ src/tail.spec.js
@@ -4,4 +4,7 @@ describe('Tail', () => {
     it('Tail of list [1,2,3] is [2,3]', () => {
         expect(tail([1,2,3])).toEqual([2,3]);
     })
+    it('Tail of list [] is null or undefined', () => {
+        expect(tail([])).toEqual(null || undefined);
+    }) 
 })
